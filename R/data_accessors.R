4e38b3550db33af56ca4f1a6840e60b29ba43237
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2022-07-05 15:35:19 -0400

    fixing S3 methods

--- R/data_accessors.R
+++ R/data_accessors.R
@@ -1,25 +1,34 @@
 
 #' @title Pull MPX Case Data
 #'
+#' @param .path Either a spoConnection or character string path to MPX data
+#' @param ... additional arguments handled by other methods (not yet implemented)
+#' 
 #' @importFrom readr read_csv
 #' @import dplyr
 #' @import tidyr
 #' @importFrom passport parse_country
-get_mpx_cases <- function(...) {
+#' 
+#' @export
+get_mpx_cases <- function(.path, ...) {
   UseMethod("get_mpx_cases")
 }
 
 
-# Pulling MPX data when an anonymous Sharepoint Connection is passed
-get_mpx_cases.spoConnection <- function(con, ...) {
+#' Pulling MPX data when an anonymous Sharepoint Connection is passed
+#' @rdname get_mpx_cases
+#' @export
+get_mpx_cases.spoConnection <- function(.path, ...) {
   warning("Not yet implemented")
 
   return(NULL)
 }
 
-# Pulling MPX data when passed a standard path
-get_mpx_cases.default <- function(path, ...) {
-  data_raw <- readr::read_csv(path, show_col_types = FALSE)
+#' Pulling MPX data when passed a standard path
+#' @rdname get_mpx_cases
+#' @export
+get_mpx_cases.default <- function(.path, ...) {
+  data_raw <- readr::read_csv(.path, show_col_types = FALSE)
 
   out <- data_raw %>%
     tidyr::pivot_longer(-Country, names_to = "date", values_to = "cases") %>%
