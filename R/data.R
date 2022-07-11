2c18d8297d4730762a11088fbceaf3313d804eb2
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2022-07-11 16:47:24 -0400

    Adding Endemic Countries file - Closes #1 - Added docs, internal version with ISO code

--- R/data.R
+++ R/data.R
@@ -7,5 +7,17 @@
 #'   \item{\code{iso3code}}{character ISO 3166-1 alpha-3 country code}
 #'   \item{\code{geometry}}{list sf geometry column}
 #' }
-#' @details DETAILS
+#' @details Liable to update, possibly (likely) incorrect spherical geometry.
 "global_map"
+
+#' @title Monkeypox Endemic Countries
+#' @description A data frame of countries that are known to be endemic for Monkeypox.
+#' @format A data frame with 10 rows and 3 variables:     
+#' \describe{
+#'   \item{\code{Country}}{character English name for administrative region}  
+#'   \item{\code{Endemic}}{character "Y"}  
+#'   \item{\code{iso3code}}{character ISO 3166-1 alpha-3 country code} 
+#'}
+#' @details 
+#' This is used internally for filtering for some tables and viz.
+"endemic_countries"
