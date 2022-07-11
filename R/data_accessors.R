61a773b3a4cae888d43163896c51ca12ce34e649
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2022-07-11 14:13:10 -0400

    Reworking get_mpx_cases() - Now calling a new function internally to make more adaptable

--- R/data_accessors.R
+++ R/data_accessors.R
@@ -1,39 +1,47 @@
-
 #' @title Pull MPX Case Data
 #'
-#' @param .path Either a spoConnection or character string path to MPX data
+#' @param path character string path to MPX data
+#' @param connection (optional) an `spoConnection` object
 #' @param ... additional arguments handled by other methods (not yet implemented)
 #' 
-#' @importFrom readr read_csv
 #' @import dplyr
 #' @import tidyr
 #' @importFrom passport parse_country
 #' 
-#' @export
-get_mpx_cases <- function(.path, ...) {
-  UseMethod("get_mpx_cases")
-}
+get_mpx_cases <- function(path, connection = NULL) {
+  raw_data <- fetch_mpx_cases(path, connection)
 
-
-#' Pulling MPX data when an anonymous Sharepoint Connection is passed
-#' @rdname get_mpx_cases
-#' @export
-get_mpx_cases.spoConnection <- function(.path, ...) {
-  warning("Not yet implemented")
-
-  return(NULL)
-}
-
-#' Pulling MPX data when passed a standard path
-#' @rdname get_mpx_cases
-#' @export
-get_mpx_cases.default <- function(.path, ...) {
-  data_raw <- readr::read_csv(.path, show_col_types = FALSE)
-
-  out <- data_raw %>%
+  out <- raw_data %>%
     tidyr::pivot_longer(-Country, names_to = "date", values_to = "cases") %>%
     mutate(
       iso3code = parse_country(Country, to = "iso3c"),
       date = as.Date(date, "%m/%d/%Y")
     )
+  
+  return(out)
+}
+
+
+
+# Helper function to handle pulling line-list data
+# (only used internally)
+fetch_mpx_cases <- function(path, connection = NULL, ...) {
+  # Dispatch based on whether we need to pull from a sharepoint site
+  # or locally
+  UseMethod("fetch_mpx_cases", connection)
+}
+
+
+# Pulling MPX data when an anonymous Sharepoint Connection is passed
+fetch_mpx_cases.spoConnection <- function(path, connection, ...) {
+  data_raw <- connection$read_file(path)
+
+  return(as_tibble(data_raw))
+}
+
+# Pulling MPX data when passed a standard path
+fetch_mpx_cases.default <- function(path, ...) {
+  data_raw <- data.table::fread(path)
+
+  return(as_tibble(data_raw))
 }
