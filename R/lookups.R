cf20e83f2998ea6f98183962cac2bc9ddf4a018c
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2022-08-09 17:24:29 -0400

    Switching to countrycode for parsing - Allows exceptions directly via lookup table - Adding lookup table of manual ISO codes for CDC purposes

---
+++ R/lookups.R
@@ -0,0 +1,6 @@
+#' Manual ISO 3166-1 alpha-3 country code entries that don't get parsed by countrycode
+manual_iso3_lk <- list(
+  Micronesia = "FSM",
+  `Saint Martin` = "MAF",
+  `Eswatini` = "SWZ"
+)
